ac2516fe272b3aac4f28f3686fb795b7dd2cda29
Adarsh Vikram Singh <103936096+AdarshVKSingh@users.noreply.github.com>
2023-05-15 19:17:59 +0530

    Added solution - LeetHub

---
+++ Division without using multiplication, division and mod operator - GFG/division-without-using-multiplication-division-and-mod-operator.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*;
+
+class GfG {
+    
+
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String S[] = br.readLine().split(" ");
+            
+            long a = Long.parseLong(S[0]);
+            
+            long b = Long.parseLong(S[1]);
+	       
+	    	Solution ob = new Solution();
+           
+            System.out.println(ob.divide(a,b));
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public static long divide(long a, long b)
+    {
+        
+        if(b==0){
+            return -1;
+        }
+        if(a==0){
+            return 0;
+        }
+        
+        return a/b;
+        
+    }
+}
