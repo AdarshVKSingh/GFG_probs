c1a5e04d712715f961e9fb723ec2e09c67009802
Adarsh Vikram Singh <103936096+AdarshVKSingh@users.noreply.github.com>
2023-08-01 18:40:36 +0530

    Added solution - LeetHub

---
+++ Smallest Positive missing number - GFG/smallest-positive-missing-number.java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    //Function to find the smallest positive number missing from the array.
+    static int missingNumber(int arr[], int size)
+    {
+        // Your code here
+        int max=0;
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        
+        for(int i:arr){
+            if(i>0 && !hm.containsKey(i)){
+                
+                hm.put(i,1);
+            }
+            
+            if(i>0 && i>max)
+            {
+                max = i;
+            }
+        }
+        
+        for(int i=1;i<=max;i++){
+            if(!hm.containsKey(i)){
+                return i;
+            }
+        }
+        
+        return max+1;
+        
+        
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class Main
+{   
+    public static void main (String[] args) throws IOException
+    {
+
+		BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter out=new PrintWriter(System.out);
+		
+        //taking testcases
+		int t=Integer.parseInt(in.readLine().trim());
+		while(t-->0){
+		    
+		    //input number n
+			int n=Integer.parseInt(in.readLine().trim());
+			int[] arr=new int[n];
+			String s[]=in.readLine().trim().split(" ");
+			
+			//adding elements to the array
+			for(int i=0;i<n;i++)
+				arr[i]=Integer.parseInt(s[i]);
+				
+			Solution obj = new Solution();
+			
+			//calling missingNumber()
+			int missing = obj.missingNumber(arr,n);
+			out.println(missing);
+		}
+		out.close();
+    }
+}
+
+
+// } Driver Code Ends
